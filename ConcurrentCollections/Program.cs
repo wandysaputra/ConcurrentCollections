40572248772a22aed85159c9eed5fce8f5889557
WandyS <wandy.saputra@hotmail.com>
2023-02-18 11:14:31 +0800

    change to ConcurrentQueue to support concurrency

--- ConcurrentCollections/Program.cs
+++ ConcurrentCollections/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Concurrent;
+
 try
 {
-    var ordersQueue = new Queue<string>();
+    var ordersQueue = new ConcurrentQueue<string>();
     var task1 = Task.Run(() => PlaceOrders(ordersQueue, "Xavier", 5));
     var task2 = Task.Run(() => PlaceOrders(ordersQueue, "Ramdevi", 5));
 
@@ -22,11 +25,11 @@ finally
     Console.ReadKey();
 }
 
-static void PlaceOrders(Queue<string> orders, string customerName, int nOrders)
+static void PlaceOrders(ConcurrentQueue<string> orders, string customerName, int nOrders)
 {
     for (int i = 1; i <= nOrders; i++)
     {
-        Thread.Sleep(1);
+        Thread.Sleep(new TimeSpan(1));
         string orderName = $"{customerName} wants t-shirt {i}";
         orders.Enqueue(orderName);
     }
