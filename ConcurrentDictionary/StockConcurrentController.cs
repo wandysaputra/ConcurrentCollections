5468a67d7d113eb03db97e22c6f4747b43a48876
WandyS <wandy.saputra@hotmail.com>
2023-02-18 14:29:04 +0800

    tidy up a bit

--- ConcurrentDictionary/StockConcurrentController.cs
+++ ConcurrentDictionary/StockConcurrentController.cs
@@ -6,17 +6,15 @@ public enum SelectResult {Success, NoStockLeft, ChosenShirtSold }
 
 public class StockConcurrentController
 {
-    // private Dictionary<string, TShirt?> _stock;
     private ConcurrentDictionary<string, TShirt?> _stock;
 
     public StockConcurrentController(IEnumerable<TShirt> shirts)
     {
-        // _stock = shirts.ToDictionary(x => x.Code);
         _stock = new ConcurrentDictionary<string, TShirt?>(shirts.ToDictionary(x => x.Code));
     }
     public bool Sell(string code)
     {
-        // _stock.Remove(code);
+        // Favor `TryXXX()` style methods, which don't presume knowledge of the state
         return _stock.TryRemove(code, out var shirtRemoved); // allows for FAILURE
     }
     public (SelectResult Success, TShirt? shirt) SelectRandomShirt()
@@ -30,15 +28,17 @@ public class StockConcurrentController
         Thread.Sleep(Rnd.NextInt(10));
         string selectedCode = keys[Rnd.NextInt(keys.Count)];
         
+        // Favor `TryXXX()` style methods, which don't presume knowledge of the state
         return _stock.TryGetValue(selectedCode, out TShirt? shirt) 
             ? (SelectResult.Success, shirt) 
             : (SelectResult.ChosenShirtSold, null);
-        // return _stock[selectedCode];
     }
     public void DisplayStock()
     {
         Console.WriteLine($"\r\n{_stock.Count} items left in stock:");
-        foreach (TShirt? shirt in _stock.Values)
+        foreach (var shirt in _stock.Values)
+        {
             Console.WriteLine(shirt);
+        }
     }
 }
