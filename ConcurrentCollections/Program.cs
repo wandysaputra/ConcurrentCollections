53976ba249e9c89ad64b74aec49b6cba03030e47
WandyS <wandy.saputra@hotmail.com>
2023-02-18 10:15:54 +0800

    Add project files.

---
+++ ConcurrentCollections/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var ordersQueue = new Queue<string>();
+PlaceOrders(ordersQueue, "Xavier", 5);
+PlaceOrders(ordersQueue, "Ramdevi", 5);
+
+foreach (string order in ordersQueue)
+    Console.WriteLine("ORDER: " + order);
+
+static void PlaceOrders(Queue<string> orders, string customerName, int nOrders)
+{
+    for (int i = 1; i <= nOrders; i++)
+    {
+        Thread.Sleep(1);
+        string orderName = $"{customerName} wants t-shirt {i}";
+        orders.Enqueue(orderName);
+    }
+}
