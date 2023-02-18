8e0ec8c7613f236361a27121547b3aa6f92f8e5f
WandyS <wandy.saputra@hotmail.com>
2023-02-18 10:40:27 +0800

    when two tasks are concurrently modifying the queue

--- ConcurrentCollections/Program.cs
+++ ConcurrentCollections/Program.cs
@@ -1,13 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+try
+{
+    var ordersQueue = new Queue<string>();
+    var task1 = Task.Run(() => PlaceOrders(ordersQueue, "Xavier", 5));
+    var task2 = Task.Run(() => PlaceOrders(ordersQueue, "Ramdevi", 5));
 
+    Task.WaitAll(task1, task2);
 
-var ordersQueue = new Queue<string>();
-PlaceOrders(ordersQueue, "Xavier", 5);
-PlaceOrders(ordersQueue, "Ramdevi", 5);
+    foreach (string order in ordersQueue)
+    {
+        Console.WriteLine("ORDER: " + order);
+    }
 
-foreach (string order in ordersQueue)
-    Console.WriteLine("ORDER: " + order);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+finally
+{
+    Console.ReadKey();
+}
 
 static void PlaceOrders(Queue<string> orders, string customerName, int nOrders)
 {
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ConcurrentCollections
+
+These might happens when two tasks are concurrently modifying the queue and `Queue<T>` is not `thread-safe`
+
+- Weird result like empty string in this case 
+- Throw exception
